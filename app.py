36989ecf46317660e8bc86c2c7339953044ab4db
unknown <Evangelos Tselepidis@DESKTOP-PFGPH9S.episystems.eu>
2023-05-25 13:27:00 +0300

    awd

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ def create_app(db_url=None):
     app = Flask(__name__)
     load_dotenv()
 
-    connection = redis.from_url(os.getenv("REDIS_URL"))
-    app.queue = Queue("emails", connection=connection)
+    # connection = redis.from_url(os.getenv("REDIS_URL"))
+    # app.queue = Queue("emails", connection=connection)
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
